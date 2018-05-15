3ebf01727e26c5b08b417a91e9f222bca73ed66b
ioanaemm <miteleaioana@gmail.com>
2018-05-15 09:37:06 +0100

    Add mobile nav

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ var crtSlide = 0;
 var slideCount = $('.slide').length;
 var MOBILE_BREAKPOINT = 768;
 var isMobile = false;
+var isMobileNavVisible = false;
 
 $(document).ready(function() {
   enableSlider();
+  enableMobileNav();
   distributeShoppingCart();
+  distributeNav();
   $(window).resize(onWindowResize);
   onWindowResize();
 });
@@ -22,6 +25,16 @@ function distributeShoppingCart() {
   $('header.mobile .shopping-cart').append(cartCloneMobile);
 
   $('.temp-cart .cart-wrapper').remove();
+}
+
+function distributeNav() {
+  var navCloneDesktop = $('.temp-nav .main-nav-list').clone();
+  $('header.desktop nav').append(navCloneDesktop);
+
+  var navCloneMobile = $('.temp-nav .main-nav-list').clone();
+  $('nav.mobile').append(navCloneMobile);
+
+  $('.temp-nav .main-nav-list').remove();
 }
 
 function enableSlider() {
@@ -49,5 +62,27 @@ function prevSlide() {
 
 function animateSlider(time) {
   var viewportWidth = $('.slide-viewport').width();
+  if(window.innerWidth < 500) {
+    time /= 2;
+  }
   TweenMax.to($('.slide-container'), time, {left: -crtSlide * viewportWidth, ease: Power2.easeInOut});
+}
+
+function enableMobileNav() {
+  $('.drawerContainer').click(toggleMobileNav);
+}
+
+function toggleMobileNav() {
+  var newMainLeft;
+  var newNavLeft;
+  if(!isMobileNavVisible) {
+    newMainLeft = 250;
+    newNavLeft = 0;
+  } else {
+    newMainLeft = 0;
+    newNavLeft = -250;
+  }
+  isMobileNavVisible = !isMobileNavVisible;
+  TweenMax.to($('main'), 0.5, {left: newMainLeft, ease: Power2.easeInOut});
+  TweenMax.to($('nav.mobile'), 0.5, {left: newNavLeft, ease: Power2.easeInOut});
 }
