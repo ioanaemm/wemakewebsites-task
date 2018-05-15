95ae3942d76eb660b6e4661f7b7751c45b2cfb9b
ioanaemm <miteleaioana@gmail.com>
2018-05-15 08:03:57 +0100

    Add slider functionality

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
+var crtSlide = 0;
+var slideCount = $('.slide').length;
+
 $(document).ready(function() {
-  console.log("ready");
+  enableSlider();
 });
+
+
+function enableSlider() {
+  $('#left-arrow').click(prevSlide);
+  $('#right-arrow').click(nextSlide);
+  $(window).resize(calibrateSlider);
+}
+
+function nextSlide() {
+  if(crtSlide < slideCount - 1) {
+    crtSlide++;
+  } else {
+    crtSlide = 0;
+  }
+  animateSlider(1.5);
+}
+
+function prevSlide() {
+  if(crtSlide > 1) {
+    crtSlide--;
+  } else {
+    crtSlide = slideCount - 1;
+  }
+  animateSlider(1.5);
+}
+
+function calibrateSlider() {
+  animateSlider(0);
+}
+
+function animateSlider(time) {
+  var viewportWidth = $('.slide-viewport').width();
+  TweenMax.to($('.slide-container'), time, {left: -crtSlide * viewportWidth, ease: Power2.easeInOut});
+}
