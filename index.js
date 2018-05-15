fd90e5ed589ab668306f1b070b93d41a4477cd91
ioanaemm <miteleaioana@gmail.com>
2018-05-15 10:03:38 +0100

    Fix mobile nav toggle

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ var slideCount = $('.slide').length;
 var MOBILE_BREAKPOINT = 768;
 var isMobile = false;
 var isMobileNavVisible = false;
+var isMobileCartVisible = false;
 
 $(document).ready(function() {
-  enableSlider();
+  initSlider();
+  initMobileCart();
   enableMobileNav();
   distributeShoppingCart();
   distributeNav();
@@ -15,6 +17,9 @@ $(document).ready(function() {
 
 function onWindowResize() {
   animateSlider(0);
+  if(isMobileNavVisible) {
+    hideMobileNav();
+  }
 }
 
 function distributeShoppingCart() {
@@ -37,9 +42,27 @@ function distributeNav() {
   $('.temp-nav .main-nav-list').remove();
 }
 
-function enableSlider() {
+function initSlider() {
   $('#left-arrow').click(prevSlide);
   $('#right-arrow').click(nextSlide);
+  addSliderMarkers();
+  refreshMarkers();
+}
+
+function addSliderMarkers() {
+  var marker = '<div class="marker"></div>';
+  for(var i = 0; i < slideCount; i++) {
+    $('.carousel .markers').append($(marker));
+  }
+}
+
+function refreshMarkers() {
+  $('.carousel .marker').removeClass('selected');
+  $('.carousel .marker').each(function(index, markerElement) {
+    if(index === crtSlide) {
+      $(markerElement).addClass('selected');
+    }
+  });
 }
 
 function nextSlide() {
@@ -49,6 +72,7 @@ function nextSlide() {
     crtSlide = 0;
   }
   animateSlider(1.5);
+  refreshMarkers();
 }
 
 function prevSlide() {
@@ -58,6 +82,7 @@ function prevSlide() {
     crtSlide = slideCount - 1;
   }
   animateSlider(1.5);
+  refreshMarkers();
 }
 
 function animateSlider(time) {
@@ -73,16 +98,34 @@ function enableMobileNav() {
 }
 
 function toggleMobileNav() {
-  var newMainLeft;
-  var newNavLeft;
-  if(!isMobileNavVisible) {
-    newMainLeft = 250;
-    newNavLeft = 0;
+  if(isMobileNavVisible) {
+    hideMobileNav();
   } else {
-    newMainLeft = 0;
-    newNavLeft = -250;
+    showMobileNav();
   }
-  isMobileNavVisible = !isMobileNavVisible;
-  TweenMax.to($('main'), 0.5, {left: newMainLeft, ease: Power2.easeInOut});
-  TweenMax.to($('nav.mobile'), 0.5, {left: newNavLeft, ease: Power2.easeInOut});
+}
+
+function showMobileNav() {
+  isMobileNavVisible = true;
+  TweenMax.to($('main'), 0.5, {left: 250, ease: Power2.easeInOut});
+  TweenMax.to($('nav.mobile'), 0.5, {left: 0, ease: Power2.easeInOut});
+}
+
+function hideMobileNav() {
+  isMobileNavVisible = false;
+  TweenMax.to($('main'), 0.5, {left: 0, ease: Power2.easeInOut});
+  TweenMax.to($('nav.mobile'), 0.5, {left: -250, ease: Power2.easeInOut});
+}
+
+function initMobileCart() {
+  $('header.mobile .shopping-cart .cart-icon').click(toggleMobileCart);
+}
+
+function toggleMobileCart() {
+  if(isMobileCartVisible) {
+    $('header.mobile .shopping-cart .cart-wrapper').hide();
+  } else {
+    $('header.mobile .shopping-cart .cart-wrapper').show();
+  }
+  isMobileCartVisible = !isMobileCartVisible;
 }
